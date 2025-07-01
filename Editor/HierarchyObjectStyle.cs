4d7b2089bd809784fcbaa9c8fe49eec48f723c94
Ty Radman <timdesign1996@gmail.com>
2025-07-01 02:38:26 -0700

    Set default color for HierarchyPrefixStyle and bump version
    Added a default red color to the Color field in HierarchyPrefixStyle. Updated package version to 1.1.6 to reflect this change.

--- Editor/HierarchyObjectStyle.cs
+++ Editor/HierarchyObjectStyle.cs
@@ -5,7 +5,7 @@ public class HierarchyPrefixStyle
 {
     public string StyleName;
     public string Prefix;
-    public Color Color;
+    public Color Color = new Color(1f, 0f, 0f, 1f);
     public Texture2D Icon;
     public NameMode Alignment;
     public string TooltipText;
