e40111e4dbc2cc7ba8b2e4a1d2534a5a48ff4909
Ty <82220311+TyRadman@users.noreply.github.com>
2026-01-19 11:27:05 -0800

    Delete HierarchyObjectsData.cs

--- Editor/HierarchyObjectsData.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public class HierarchyObjectsData : ScriptableObject
-{
-    public List<HierarchyPrefixStyle> Styles = new List<HierarchyPrefixStyle>();
-}
